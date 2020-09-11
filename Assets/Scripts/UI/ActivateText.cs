27e0e050bb5d14d254465351b48f3a14579e97a8
JamesZhaoCCC <zhaojames280@gmail.com>
2020-09-11 14:28:53 -0400

    Text-JamesZhao

---
+++ Assets/Scripts/UI/ActivateText.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateText : MonoBehaviour
+{
+    public TextAsset theText;
+
+    public int start;
+    public int end;
+
+    public TextBoxManager textbox;
+
+    public bool onbuttonpress;
+    private bool waitforpress;
+
+    public bool CloseWhenDone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = FindObjectOfType<TextBoxManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waitforpress && Input.GetKeyDown(KeyCode.Y))
+        {
+            textbox.reuse(theText);
+            textbox.currentline = start;
+            textbox.endatline = end;
+            textbox.enable();
+
+            if (CloseWhenDone)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            if(onbuttonpress)
+            {
+                waitforpress = true;
+                return;
+            }
+            textbox.reuse(theText);
+            textbox.currentline = start;
+            textbox.endatline = end;
+            textbox.enable();
+
+            if(CloseWhenDone)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            waitforpress = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/TextBoxManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textbox;
+
+    public Text theText;
+
+    public TextAsset textfile;
+    public string[] textlines;
+
+    public int currentline;
+    public int endatline;
+
+    //public PlayerController player;
+
+    public bool isActive;
+    
+    void Start()
+    {
+        //player = FindObjectOfType<PlayerController>();
+
+        if (textfile != null)
+        {
+            textlines = (textfile.text.Split('\n'));
+        }
+
+        if(endatline == 0) //if endline is unspecified, go to the end
+        {
+            endatline = textlines.Length - 1;
+        }
+
+        if(isActive)
+        {
+            enable();
+        }
+        else
+        {
+            disable();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(!isActive)
+        {
+            return;
+        }
+        
+        theText.text = textlines[currentline];
+
+        if(Input.GetKeyDown(KeyCode.Return)) // press return/enter advances text
+        {
+            currentline += 1;
+        }
+
+        if(currentline > endatline) // once you reach the end, box disappears
+        {
+            disable();
+        }
+    }
+
+    public void enable() //add textbox
+    {
+        textbox.SetActive(true);
+        isActive = true;
+    }
+
+    public void disable() //take away textbox
+    {
+        textbox.SetActive(false);
+        isActive = false;
+    }
+
+    public void reuse(TextAsset theText) //reuse textbox
+    {
+        if(theText != null)
+        {
+            textlines = new string[1];
+            textlines = (theText.text.Split('\n'));
+        }
+    }
+}
