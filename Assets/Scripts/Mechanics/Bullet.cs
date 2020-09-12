c4f6edc4a37a7945a7fcbd34a63157fbd6959ad8
Phillip Chen <Philman84@gmail.com>
2020-09-11 21:36:24 -0400

    A script for one of the properties of Bullet_0 Prefabs

---
+++ Assets/Scripts/Mechanics/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public float speed = 10.0f;
+	public Rigidbody2D rb;
+	public Vector2 movement;
+	private Vector2 screenBounds;
+	void Start(){
+		screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+		if(transform.position.x > screenBounds.x * 1.2){
+            Destroy(this.gameObject);
+        }
+		movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+	}
+
+	void FixedUpdate(){
+		moveCharacter(movement);
+	}
+
+	void moveCharacter(Vector2 direction){
+		rb.MovePosition((Vector2)transform.position + new Vector2(speed * Time.deltaTime,0f));
+	}
+	
+}
